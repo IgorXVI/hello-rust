23710605e4061a0ddb6287e03e0a89c5062c5888
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2023-06-08 04:14:15 -0300

    references immutable

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,18 @@
 fn main() {
-    //Ownership rules
-    //1. Each value in Rust has a variable that's called its owner.
-    //2. There can only be one owner at a time.
-    //3. When the owner goes out of scope, the value will be dropped.
+    //The Rules of References
+    //
+    //1. At any given time, you can have either one mutable reference or
+    //any number of immutable references.
+    //
+    //2. References must point to valid data.
 
-    let s = String::from("hello");
-    takes_ownership(s); //s moved to the scope of the function takes_ownership, s is no longer valid
+    let s1 = String::from("cabeça de cavalo");
+    let len = calculate_length(&s1);
 
-    //the following line will result in a compilation error "borrow of moved value"
-    //println!("{s}");
-
-    let x = 10;
-    makes_copy(10); // primitive values are copied by default
-    println!("{x}");
-
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-
-    println!("s1 = {s1}, s3 = {s3}");
+    println!("The length of \"{s1}\" is {len}");
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
-}
-
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
-}
-
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: &String) -> usize {
+    let len = s.len();
+    len
 }
