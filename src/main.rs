c708968be510c435bc64778dcd94891494f26870
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2023-06-08 02:51:48 -0300

    variables shadowing and const

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
-use rand::Rng;
-
-use colored::*;
-
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    let secret = rand::thread_rng().gen_range(1, 101);
+    let x = 5;
 
-    println!("The secret is {secret}");
+    println!("{}", x);
 
-    loop {
-        println!("Please input your guess.");
+    let x = "mamao";
 
-        let mut guess = String::new();
+    println!("{}", x);
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    const BANANA_COUNT: u32 = 100_000;
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret) {
-            Ordering::Less => println!("{}", "Too small!".red()),
-            Ordering::Greater => println!("{}", "Too big!".red()),
-            Ordering::Equal => {
-                println!("{}", "You win!".green());
-                break;
-            }
-        }
-    }
+    println!("{}", BANANA_COUNT);
 }
