2232a0d92eb300967fd86254175ad652b5c6300e
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2023-06-08 03:03:19 -0300

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
 fn main() {
-    //INTEGER
+    let sum = my_function(11, 22);
 
-    let a = 98_222; //Decimal
-    let b = 0xff; //Hex
-    let c = 0o77; //Octal
-    let d = 0b1111_0001; //Binary
-    let e = b'A'; //Byte (u8 only)
+    println!("the sum is {sum}");
+}
 
-    let f: u8 = 255;
+fn my_function(x: i32, y: i32) -> i32 {
+    println!("The value of x is {x}");
+    println!("The value of y is {y}");
 
-    //Floating-point numbers
-
-    let g = 2.0;
-    let h: f32 = 3.0;
-
-    //Tuples
-
-    let tup = ("banana", 100_000);
-
-    let (fruit, count) = tup;
-
-    let count = tup.1;
-
-    //Arrays
-
-    let error_codes = [200, 404, 500];
-    let not_found = error_codes[1];
-
-    //8 values all set to zero
-    let byte = [0; 8];
-
+    x + y
 }
